57928319b047293c0994b8d81c456bd997d12321
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2019-08-05 14:17:18 +0200

    cleanup

--- util/netcdf_io.py
+++ util/netcdf_io.py
@@ -13,18 +13,15 @@ def nc4_get_copy(name_in, name_out):
     # Copy variables
     for v_name, in_var in din.variables.items():
         # always create double precision output. Default is single which will not work for perturb.
-        #out_var = dout.createVariable(v_name, in_var.datatype, in_var.dimensions)
         out_var = dout.createVariable(v_name, np.float64, in_var.dimensions)
 
-        # Copy variable attributes
-        ncdict = {k: in_var.getncattr(k) for k in in_var.ncattrs() if k not in ["_FillValue", "missing_value"]}
+        # Copy variable attributes, make sure float32 attributes are created as float64
+        real_atts = [k for k in in_var.ncattrs() if isinstance(in_var.getncattr(k), np.float32)]
+        ncdict = {k: in_var.getncattr(k) for k in in_var.ncattrs() if k not in real_atts}
+        ncdict_dbl = {k: np.float64(in_var.getncattr(k)) for k in in_var.ncattrs() if k in real_atts}
+        out_var.setncatts({**ncdict, **ncdict_dbl})
 
-        for k in ["_FillValue", "missing_value"]:
-            if k in in_var.ncattrs():
-                ncdict[k] = np.float64(in_var.getncattr(k))
-        out_var.setncatts(ncdict)
-
-        out_var[:] = in_var[:]
+        out_var[:] = np.float64(in_var[:])
 
     # close the input file
     din.close()
