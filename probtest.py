909178b4262e0dac0ed00c4a68ac7d882d9a1ea7
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2019-08-16 17:36:47 +0200

    Set default paths from command line

--- probtest.py
+++ probtest.py
@@ -17,14 +17,29 @@ def make_parser():
     """
     parser = argparse.ArgumentParser(description=description)
     parser.add_argument('config', metavar='config.cfg', type=str, help='The configuration file')
-    parser.add_argument('mode', metavar='perturb/stats/check/tolerance', type=str, nargs='*',
+    parser.add_argument('mode', metavar='mode', type=str, nargs='*',
                         help='The mode to be run in')
+    parser.add_argument('-i', dest='model_input_dir', type=str, help='the model input directory')
+    parser.add_argument('-o', dest='model_output_dir', type=str, help='the model output directory')
+    parser.add_argument('-t', dest='tolerance_file_name', type=str, help='the full path to the tolerance file')
+    parser.add_argument('--ref', dest='input_file_ref', type=str, help='check: the path to the reference file')
+    parser.add_argument('--cur', dest='input_file_cur', type=str, help='check: the path to the current file')
+    parser.add_argument('-r', dest='model_run_dir', type=str, help='the path to where to model is launched from')
 
     return parser.parse_args()
 
 
+def make_default_dict_from_parser(args):
+    default_dict = vars(args)
+    default_dict = {key: val for key, val in default_dict.items() if key not in ['config', 'mode'] and val}
+    print(default_dict)
+    return default_dict
+
+
 def main(args):
-    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation())
+    default_dict = make_default_dict_from_parser(args)
+
+    config = configparser.ConfigParser(interpolation=configparser.ExtendedInterpolation(), defaults=default_dict)
     config.read(args.config)
 
     mode = args.mode
