5763be7ae2d65e14325f4a8fc5f62a6f8d5c2cc6
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2019-08-22 17:46:31 +0200

    Change exp-name to run ensemble

--- engine/run_ensemble.py
+++ engine/run_ensemble.py
@@ -1,20 +1,23 @@
 import os
 import subprocess
 import time
+import re
 
 from util.constants import perturbed_model_input_dir, perturbed_model_output_dir
 
 
-def prepare_perturbed_run_script(in_file_name, out_file_name, init_key, ini_value,
-                                 output_key, output_value, d_in, d_out):
+def prepare_perturbed_run_script(in_file_name, out_file_name, init_key, init_value,
+                                 d_in, old_exp, new_exp):
     in_file = open(in_file_name, 'r')
     out_file = open(out_file_name, 'w')
 
     for line in in_file:
-        if (init_key in line) and (ini_value in line) and not line.strip().startswith("#"):
+        # replace input directory with the one given in config file
+        if (init_key in line) and (init_value in line) and not line.strip().startswith("#"):
             line = "{}={}\n".format(init_key, d_in)
-        elif (output_key in line) and (output_value in line) and not line.strip().startswith("#"):
-            line = "{}={}\n".format(output_key, d_out)
+        # rename the experiment name
+        elif old_exp in line and not line.strip().startswith("#"):
+            line = re.sub(old_exp, new_exp, line)
         out_file.write(line)
 
     print("writing model run script to: {}".format(out_file_name))
@@ -30,7 +33,7 @@ def run_ensemble(config):
     model_output_dir = config.get("model_output_dir")
     submit_command = config.get("submit_command")
     init_key, init_val = config.get("init_keyval").split(",")
-    output_key, output_val = config.get("output_keyval").split(",")
+    old_exp, new_exp = config.get("rename_exp").split(",")
     seeds = config.get("seeds").split(",")
     parallel = config.getboolean("parallel")
     dry = config.getboolean("dry")
@@ -38,18 +41,16 @@ def run_ensemble(config):
     os.chdir(model_run_dir)
     procs = []
     for s in seeds:
+        seed_extension = "_seed_{}".format(s)
         d_in = "{}/{}".format(model_output_dir, perturbed_model_input_dir.format(seed=s))
-        d_out = "{}/{}".format(model_output_dir, perturbed_model_output_dir.format(seed=s))
         in_file_name = "{}/{}".format(model_run_dir, model_run_script_name.format(mod=''))
-        out_file_name = "{}/{}".format(model_run_dir, model_run_script_name.format(mod="_seed_{}".format(s)))
+        out_file_name = "{}/{}".format(model_run_dir, model_run_script_name.format(mod=seed_extension))
+        new_exp = new_exp.format(mod=seed_extension)
 
         run_script = prepare_perturbed_run_script(in_file_name, out_file_name,
-                                                  init_key, init_val, output_key, output_val, d_in, d_out)
+                                                  init_key, init_val, d_in, old_exp, new_exp)
 
         run_script = os.path.basename(run_script)
-        if not os.path.exists(d_out):
-            print("creating perturbed model output directory {}".format(d_out))
-            os.makedirs(d_out)
         cmd_list = submit_command.format(seed=s).split(" ") + [run_script]
         if not dry:
             p = subprocess.Popen(cmd_list)
