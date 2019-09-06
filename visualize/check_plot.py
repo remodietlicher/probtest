b0dd03485967dc846f2827eb496ad6e53369f526
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2019-09-06 17:21:50 +0200

    nicer check plot

--- visualize/check_plot.py
+++ visualize/check_plot.py
@@ -3,6 +3,7 @@ import matplotlib.pyplot as plt
 from util.constants import compute_statistics, MODE_TOLERANCE, MODE_CHECK, MODE_VISUALIZE
 from engine.tolerance import compute_max_rel_diff_dataframe
 import numpy as np
+import matplotlib.lines as mlines
 
 prop_cycle = plt.rcParams['axes.prop_cycle']
 colors = prop_cycle.by_key()['color']
@@ -20,29 +21,36 @@ def check_plot(config):
     cur_df = pd.read_csv(input_file_cur)
     diff_df = compute_max_rel_diff_dataframe(ref_df, cur_df, check_variable_names)
 
+    ncols = min(len(check_variable_names), 3)
     nrows = int(np.ceil(len(check_variable_names) / 3.0))
-    fig, ax = plt.subplots(ncols=3, nrows=nrows, figsize=(16, 3*nrows), sharey=True, sharex=True)
+    fig, ax = plt.subplots(ncols=ncols, nrows=nrows, figsize=(5*ncols, 3.5 * nrows), sharey=True, sharex=True)
     if nrows == 1:
         ax = np.expand_dims(ax, axis=0)
+    if ncols == 1:
+        ax = np.expand_dims(ax, axis=1)
 
     for i, vn in enumerate(check_variable_names):
         tol = tol_df[tol_df['name'] == vn]
         diff = diff_df[diff_df['name'] == vn]
         for j, c in enumerate(compute_statistics):
-            ax[i // 3, i % 3].semilogy(tol['ntime'].values, tol[c].values, label='{} {}'.format(vn, c),
-                                       c=colors[j])
-            ax[i // 3, i % 3].semilogy(diff['ntime'].values, diff[c].values, label='{} {}'.format(vn, c), ls='--',
-                                       c=colors[j])
-            ax[i // 3, i % 3].legend()
+            ax[i // 3, i % 3].semilogy(tol['ntime'].values, tol[c].values, c=colors[j])
+            ax[i // 3, i % 3].semilogy(diff['ntime'].values, diff[c].values, ls='--', c=colors[j])
             ax[i // 3, i % 3].set_ylim(bottom=1e-15, top=1)
             ax[i // 3, i % 3].set_xlabel("timestep")
             ax[i // 3, i % 3].set_title(vn)
-            if vn == 'zg':
-                print(diff[c].values)
         if i % 3 == 0:
             ax[i // 3, 0].set_ylabel("relative error")
 
+    lines = []
+    for i, c in enumerate(compute_statistics):
+        lines.append(mlines.Line2D([], [], color=colors[i], label=c))
+    lines.append(mlines.Line2D([], [], color='k', label='tolerance'))
+    lines.append(mlines.Line2D([], [], color='k', ls='--', label='model'))
+
+    legcol = 3 if ncols == 1 else 5
+    ax[-1, ncols // 2].legend(handles=lines, loc='upper center', bbox_to_anchor=(0.5, -0.25), ncol=legcol)
     fig.tight_layout()
+    fig.subplots_adjust(bottom=0.3)
     if savedir:
         path = '{}/{}'.format(savedir, 'check_plot.pdf')
         print("saving figure to {}".format(path))
