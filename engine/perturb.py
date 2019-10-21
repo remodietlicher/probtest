4edc74e90c033bfd4ef63f2a757db8ce9e2db17f
Remo Dietlicher <remo.dietlicher@meteoswiss.ch>
2019-10-21 08:10:56 +0200

    copy utility files in input folder

--- engine/perturb.py
+++ engine/perturb.py
@@ -1,15 +1,24 @@
 import os
+import shutil
 from util.netcdf_io import nc4_get_copy
 import numpy as np
 
 
-def create_perturb_files(in_path, in_files, out_path):
+def create_perturb_files(in_path, in_files, out_path, copy_all_files=False):
     path = os.path.abspath(in_path)
     if not os.path.exists(out_path):
         print("creating new directory: {}".format(out_path))
         os.makedirs(out_path)
     data = [nc4_get_copy("{}/{}".format(path, f), "{}/{}".format(out_path, f))
-            for f in in_files.split(",")]
+            for f in in_files]
+
+    if copy_all_files:
+        all_files = os.listdir(path)
+        # disregard the input files which are copied above
+        other_files = [f for f in all_files if f not in in_files]
+        # copy all other files
+        for f in other_files:
+            shutil.copy(f, out_path)
 
     return data
 
@@ -25,14 +34,15 @@ def perturb_array(array, s, a):
 def perturb(config):
     model_input_dir = config.get("model_input_dir")
     perturbed_model_input_dir = config.get("perturbed_model_input_dir")
-    files = config.get("files")
+    files = config.get("files").split(",")
     seeds = np.array(config.get("seeds").split(",")).astype(int)
     variable_names = config.get("variable_names").split(",")
     amplitude = config.getfloat("amplitude")
+    copy_all_files = config.getbool("copy_all_files")
 
     for s in seeds:
         perturbed_model_input_dir_seed = perturbed_model_input_dir.format(seed=s)
-        data = create_perturb_files(model_input_dir, files, perturbed_model_input_dir_seed)
+        data = create_perturb_files(model_input_dir, files, perturbed_model_input_dir_seed, copy_all_files)
         for d in data:
             for vn in variable_names:
                 d.variables[vn][:] = perturb_array(d.variables[vn][:], s, amplitude)
